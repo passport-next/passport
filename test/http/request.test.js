f8aabfddbcbd32649cb82c05e8a5bc7782b73039
Iyanu-Tomiwa <iyanu.durotola@andela.co>
2018-09-18 18:08:30 +0100

    Removed monkeypatching http

--- test/http/request.test.js
+++ test/http/request.test.js
@@ -6,7 +6,7 @@
 const http = require('http');
 const Passport = require('../..').Passport;
 
-require('../../lib/framework/connect').__monkeypatchNode();
+require('../../lib/framework/connect')();
 
 
 describe('http.ServerRequest', () => {
