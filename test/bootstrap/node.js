7a93256c368bf77105c59f1869a220f157ad9ffd
Rowan Wookey <admin@rwky.net>
2019-08-26 11:40:30 +0100

    Updated npm deps and updated from passport-next/skel

--- test/bootstrap/node.js
+++ test/bootstrap/node.js
@@ -2,7 +2,7 @@
 
 const chai = require('chai');
 
-chai.use(require('chai-connect-middleware'));
+chai.use(require('@passport-next/chai-connect-middleware'));
 chai.use(require('@passport-next/chai-passport-strategy'));
 
 global.expect = chai.expect;
